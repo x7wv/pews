12f4b66c2b6965a388c78259f2f2818f9fc88cb9
x7wv <dansvarch10@hotmail.com>
2026-07-29 03:07:56 -0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -92,6 +92,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/pews-logo.png", type: "image/png" },
+      { rel: "apple-touch-icon", href: "/pews-logo.png" },
     ],
   }),
   shellComponent: RootShell,
