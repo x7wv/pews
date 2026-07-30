a24b151d3e151ae0d1e4ef32ed1adda931f5c911
x7wv <dansvarch10@hotmail.com>
2026-07-29 22:44:05 -0700

    Add files via upload

---
+++ src/lib/fonts.ts
@@ -0,0 +1,12 @@
+export const FONT_OPTIONS = [
+  "Space Grotesk",
+  "Inter",
+  "JetBrains Mono",
+  "Poppins",
+  "Playfair Display",
+  "Bebas Neue",
+  "Roboto Mono",
+  "Outfit",
+  "DM Serif Display",
+  "Chakra Petch",
+] as const;
